237a3aeea330badf0262bf0a82645dffd0f2caf6
Nitishcodelab <nitish1395brh@gmail.com>
2026-09-06 10:08:02 +0530

    Add form submission alert and welcome message

--- script.js
+++ script.js
@@ -2,3 +2,11 @@ function hello() {
     document.getElementById("message").innerText =
         "Welcome Nitish! 🎉 तुम coding सीख रहे हो।";
 }
+
+const forms = document.querySelectorAll("form");
+
+forms[1].addEventListener("submit", function(event) {
+    event.preventDefault();
+
+    alert("Login button काम कर रहा है! 🎉");
+});
