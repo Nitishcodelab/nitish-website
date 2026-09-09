82390b66d6376e3dbec8f2fb84f46326a6223f86
Nitishcodelab <nitish1395brh@gmail.com>
2026-09-09 11:22:17 +0530

    Update script.js

--- script.js
+++ script.js
@@ -8,13 +8,15 @@ import {
     signOut
 } from "https://www.gstatic.com/firebasejs/12.18.0/firebase-auth.js";
 
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyAuS1ECRUsfQSC7HCrChNhLV87LEmNqYto",
-  authDomain: "...",
-  projectId: "...",
-  storageBucket: "...",
-  messagingSenderId: "...",
-  appId: "..."
+  authDomain: "nitish-website.firebaseapp.com",
+  projectId: "nitish-website",
+  storageBucket: "nitish-website.firebasestorage.app",
+  messagingSenderId: "1035429607659",
+  appId: "1:1035429607659:web:a71a0fca2e722102a5e6ae",
+  measurementId: "G-LM97DLSJ1Z"
 };
 
 const app = initializeApp(firebaseConfig);
